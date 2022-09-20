79734f5d7beae0bb0b95c134785e051038c110ec
gargmanisha890 <100961998+gargmanisha890@users.noreply.github.com>
2022-09-20 19:59:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
 
 
-reportWebVitals();
 
+ReactDOM.render(<App />, document.getElementById("root"));
